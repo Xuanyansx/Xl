82ab1bb6bd01c51ff58b3366545d6ae3fa11dc35
Xunayansx <zxcvbnm629629mc@163.com>
2026-03-04 11:15:14 +0800

    修复了一个小bug

--- py/蓝桥杯/（综合）进制计数器.py
+++ py/蓝桥杯/（综合）进制计数器.py
@@ -35,10 +35,10 @@ while True:
     nk = input(f"{n} 是什么进制的")
     k = input("处理成 2 8 10 16 进制")
 
-    if nk !='10':
-        res = k_to_shi(n,nk)
-        if k == '10':
-            print(res)
-        else:
-            res = shi_to_k(res,k)
-            print(res)
+
+    res = k_to_shi(n,nk)
+    if k == '10':
+        print(res)
+    else:
+        res = shi_to_k(res,k)
+        print(res)
