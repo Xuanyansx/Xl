8295bf1b71d71084b2a1d80aec070c13c8eaf5f7
Xunayansx <zxcvbnm629629mc@163.com>
2026-04-02 20:52:14 +0800

    原来是记忆相同的状态

--- py/蓝桥杯/地堡取宝.py
+++ py/蓝桥杯/地堡取宝.py
@@ -11,41 +11,35 @@ n,m,k = get_input()
 Map = []
 ans = 0
 
-def dfs(x,y,o,w):
 
+
+@lru_cache(None)
+def dfs(x,y,o,w):
+    temp_w = Map[y][x]
+    ans = 0
     if x==m-1 and y==n-1:
-        if  o==k :
-            global ans
-            ans+=1
-            print(path)
-        return
-    
+        if o == k:
+            return 1
+        if temp_w>w and o==k-1:
+            return 1
+        return 0    
+            
+
 
     for i,j in [(0,1),(1,0)]:
         xx,yy = x+i,y+j
         if xx>=m or yy>=n:
             continue
-        temp_w = Map[yy][xx]
 
-        path.append(temp_w)
-
-        if temp_w>w:
+        if temp_w > w:
             if o<k:
-                dfs(xx,yy,o+1,temp_w)
-            dfs(xx,yy,o,w)
-        else:
-            dfs(xx,yy,o,w)
-        path.pop(-1) 
-        
-         
-
+                ans += dfs(xx,yy,o+1,temp_w)
+        ans+=dfs(xx,yy,o,w)
+    t = (10**9)+7 
+    return ans%t
 
 for i in range(n):
     Map.append(get_input())
 
-
-path = []
-dfs(0,0,0,-1)
-dfs(0,0,1,Map[0][0])
-
+ans = dfs(0,0,0,-1)
 print(ans)
